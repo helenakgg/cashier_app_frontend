4c7a197d34c393050a467a90a81dd73a5260cd3d
helenakgg <122775236+helenakgg@users.noreply.github.com>
2023-08-18 10:32:13 +0700

    Update: Improve Validation Logic

--- src/store/slices/auth/validation.js
+++ src/store/slices/auth/validation.js
@@ -3,26 +3,27 @@ import YupPassword from 'yup-password';
 YupPassword(Yup);
 
 export const loginValidationSchema = Yup.object({
-    username : Yup.string()
-        .required("Username is required."),
     password : Yup.string()
-        .required("Password is required.")
+    .required("Password is required."),
+    username : Yup.string()
+    .required("Username is required."),
 })
 
 export const forgotPasswordValidationSchema = Yup.object({
     email : Yup.string()
+        .email("Invalid email format.")
         .required("Email is required.")
 })
 
 export const resetPasswordValidationSchema = Yup.object({
-    token : Yup.string()
-        .required("OTP is required."),
+    confirmPassword : Yup.string()
+        .required("Confirm Password is required.")
+        .oneOf([Yup.ref("newPassword"), null], "Password must match."),
     newPassword : Yup.string()
         .required("New Password is required.")
         .min(6, "Password must be at least 6 characters, 1 symbol, 1 uppercase.")
         .minUppercase(1, "Password must has at least 1 uppercase letter.")
         .minSymbols(1, "Password must has at least 1 symbol."),
-    confirmPassword : Yup.string()
-        .required("Confirm Password is required.")
-        .oneOf([Yup.ref("newPassword"), null], "Password must match."),
+    token : Yup.string()
+        .required("OTP is required."),
 })
